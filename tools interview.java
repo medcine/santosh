9cd261aa609ea2739b294c21cda336ea9f072fc6
medcine <santosh.tester87@gmail.com>
2017-03-30 18:17:35 -0700

    Add files via upload

---
+++ tools interview.java
@@ -0,0 +1,14 @@
+Selenium WebDriver
+SOAP UI.
+Maven Page Object Model (POM).
+REST services using PostMan
+Selenium GRID
+Track defects and log them into Bugzilla tool and 
+kif using swift
+qtp/uft
+appium tool
+web using see test
+python scripting
+perfecto tool
+junitjenkins 
+vstl
